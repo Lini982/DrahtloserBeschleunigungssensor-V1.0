b4c32c55fb831cde4cf194a2ccbdf69a800003ac
Lini982 <straubinator98@gmail.com>
2017-07-31 20:44:06 +0200

    organized imports

--- GUI-SLA-V1.0/src/Main.java
+++ GUI-SLA-V1.0/src/Main.java
@@ -1,15 +1,9 @@
 import java.awt.BorderLayout;
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GraphicsEnvironment;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.UIManager;
 
 public class Main {
